0b1ba85f3fb0c6efb9f16330b4b0f3ee89d761b2
Iago <iagofalcom@hotmail.com>
2019-10-13 16:02:40 -0300

    Criando as Routes das Paginas

--- src/router/routes.js
+++ src/router/routes.js
@@ -5,7 +5,21 @@ const routes = [
     component: () => import('layouts/Layout.vue'),
     children: [
       { path: '', component: () => import('pages/PageTodo') },
-      { path: '/settings', component: () => import('pages/PageSettings') }
+      { path: '/settings', component: () => import('pages/PageSettings') },
+      { path: '/answer', component: () => import('pages/Answer') },
+      { path: '/course', component: () => import('pages/Course') },
+      { path: '/discipline', component: () => import('pages/Discipline') },
+      { path: '/evaluation', component: () => import('pages/Evaluation') },
+      { path: '/institution', component: () => import('pages/Institution') },
+      { path: '/institutiontype', component: () => import('pages/InstitutionType') },
+      { path: '/local', component: () => import('pages/Local') },
+      { path: '/option', component: () => import('pages/Option') },
+      { path: '/period', component: () => import('pages/Period') },
+      { path: '/question', component: () => import('pages/Question') },
+      { path: '/role', component: () => import('pages/Role') },
+      { path: '/scalalikert', component: () => import('pages/ScalaLikert') },
+      { path: '/student', component: () => import('pages/Student') },
+      { path: '/user', component: () => import('pages/User') }
     ]
   }
 ]
